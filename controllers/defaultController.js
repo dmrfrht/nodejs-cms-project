8c79599f7e0b4155d8f9f3eeeaddad950a62148b
dmrfrht <dmrfrht1@gmail.com>
2021-02-10 11:16:00 +0300

    register user

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,12 +1,15 @@
-const Post = require('../Models/PostModel').Post;
-const Category = require('../Models/CategoryModel').Category;
+const Post = require('../Models/PostModel').Post
+const Category = require('../Models/CategoryModel').Category
+const User = require('../Models/UserModel').User
+
+const bcrypt = require('bcryptjs')
 
 module.exports = {
   index: async (req, res) => {
     const posts = await Post.find().lean()
     const cats = await Category.find().lean()
 
-    res.render('default/index', { posts: posts, cats: cats })
+    res.render('default/index', {posts: posts, cats: cats})
   },
 
   getLogin: (req, res) => {
@@ -22,6 +25,53 @@ module.exports = {
   },
 
   postRegister: (req, res) => {
-    res.send('tebrikler kayit oldunuz..')
+    let errors = []
+
+    if (!req.body.firstName) {
+      errors.push({message: 'First name is mandatory'})
+    }
+
+    if (!req.body.lastName) {
+      errors.push({message: 'Last name is mandatory'})
+    }
+
+    if (!req.body.email) {
+      errors.push({message: 'Email field is mandatory'})
+    }
+
+    if (req.body.password !== req.body.passwordConfirm) {
+      errors.push({message: 'Passwords do not match'})
+    }
+
+    if (errors.length > 0) {
+      res.render('default/register', {
+        errors: errors,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email
+      })
+    } else {
+      User.findOne({ email: req.body.email })
+        .then(user => {
+          if (user) {
+            req.flash('error-message', 'Email already exists, try to login')
+            res.redirect('/login')
+          } else {
+            const newUser = new User(req.body)
+            bcrypt.genSalt(10, (err, salt) => {
+              bcrypt.hash(newUser.password, salt, (err, hash) => {
+                newUser.password = hash
+                newUser.save()
+                  .then(user => {
+                    req.flash('success-message', 'You are now registered')
+                    res.redirect('/login')
+                  })
+              })
+            })
+
+          }
+        })
+    }
+
   },
 }
